a297fac0318e30cbd96ceab5e66e2340e3580e5d
Shamus French <36057047+ShamusYo@users.noreply.github.com>
2019-04-02 16:31:57 -0400

    Update SmoothieShop.java
    Passed SmoothieShop object into other classes to set up reset functionality
    Formatted text

--- Smoothie POS interface/Smoothie_Shop/src/smoothie_shop/SmoothieShop.java
+++ Smoothie POS interface/Smoothie_Shop/src/smoothie_shop/SmoothieShop.java
@@ -2,8 +2,6 @@ package smoothie_shop;
 
 import java.lang.Math;
 import java.text.DecimalFormat;
-
-import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -11,91 +9,94 @@ import java.awt.GridLayout;
 
 /* Author:				Shamus French
  * Creation Date:		20190325
- * Last Date Modified: 	20190327
+ * Last Date Modified: 	20190401
  */
 
-public class SmoothieShop extends Frame {
-	private static final long serialVersionUID = 1L;
+
+
+public class SmoothieShop {
 	
 	public static double preTaxTotal = 0;
 	public static double postTaxTotal = 0;
+	public static double change = 0;
+	
+	SmoothieShop tempShop;
 	
 	/* Objects for the GUI */
-	private JFrame javaFrame = new JFrame("Cyber Smoothie");
+	JFrame javaFrame = new JFrame("Cyber Smoothie");
 	
-	private Label welcomeLabel = new Label("Welcome to Cyber Smoothie!");
-	private Button btStOrder = new Button("Start New Order");
+	JLabel welcomeLabel = new JLabel("Welcome to Cyber Smoothie!", SwingConstants.CENTER);
+	JButton btStOrder = new JButton("Start New Order");
 	
-	private Label typeLabel = new Label("What type of smoothie would you like?");
-	private String [] smooth;
-	private JComboBox smoothieType;
+	JLabel typeLabel = new JLabel("What type of smoothie would you like?", SwingConstants.CENTER);
+	String [] smooth;
+	JComboBox smoothieType;
 	
-	private Label sizeLabel = new Label("What size would you like?");
-	private String [] size;
-	private JComboBox sizeType;
+	JLabel sizeLabel = new JLabel("What size would you like?", SwingConstants.CENTER);
+	String [] size;
+	JComboBox sizeType;
 	
-	private Label sweetenerLabel = new Label("What kind of sweetener would you like?");
-	private String [] sweetener;
-	private JComboBox sweetType;
+	JLabel sweetenerLabel = new JLabel("What type of sweetener would you like?", SwingConstants.CENTER);
+	String [] sweetener;
+	JComboBox sweetType;
 	
-	private Label fruitLabel = new Label("Would you like to add a fruit?");
-	private String [] fruit;
-	private JComboBox fruitType;
+	JButton btAdd = new JButton("Add current smoothie to order");
 	
-	private Button btAdd = new Button("Add current smoothie to order");
+	JLabel ordSumLabel = new JLabel("Order Summary");
+	TextArea orderSummary = new TextArea();
+	JTextField total = new JTextField("", 60);
 	
-	private Label ordSumLabel = new Label("Order Summary");
-	private TextArea orderSummary = new TextArea(10, 40);
-	private TextField total = new TextField("", 60);
+	JLabel payLabel = new JLabel("How would you like to pay?", SwingConstants.CENTER);
+	JButton btPayProc = new JButton("Proceed to Payment");
+	String [] payment;
+	JComboBox paymentType;
 	
-	private Button btPayProc = new Button("Proceed to Payment");
-	private String [] payment;
-	private JComboBox paymentType;
-	
-	// add comment
+	// Create an object of each class to call from
 	AcaiBerrySmoothie acaiBerrySmoothie = new AcaiBerrySmoothie();
 	StrawberrySmoothie strawberrySmoothie = new StrawberrySmoothie();
 	BananaSmoothie bananaSmoothie = new BananaSmoothie();
 	
 	/* Create a new SmoothieClient window */
 	@SuppressWarnings({ "deprecation", "unchecked" })
-	
-	public SmoothieShop() {
-		super("Cyber Smoothie client");
+	public SmoothieShop() {		
+		
+		/* Format the text font and size */
+		welcomeLabel.setFont(new Font("Serif", Font.BOLD, 50));
+		typeLabel.setFont(new Font("Serif", Font.BOLD, 20));
+		sizeLabel.setFont(new Font("Serif", Font.BOLD, 20));
+		sweetenerLabel.setFont(new Font("Serif", Font.BOLD, 20));
+		payLabel.setFont(new Font("Serif", Font.BOLD, 20));
 		
 		/* Create the panel for holding all fields*/
 		Panel startPanel = new Panel(new BorderLayout());
-		Panel typePanel = new Panel(new BorderLayout());
-		Panel sizePanel = new Panel(new BorderLayout());
-		Panel sweetenerPanel = new Panel(new BorderLayout());
-		Panel fruitPanel = new Panel(new BorderLayout());
+		Panel typePanel = new Panel(new GridLayout(1, 2, 20, 20));
+		Panel sizePanel = new Panel(new GridLayout(1, 2, 20, 20));
+		Panel sweetenerPanel = new Panel(new GridLayout(1, 2, 20, 20));
 		Panel ordSumPanel = new Panel(new BorderLayout());
 		Panel addPanel = new Panel(new BorderLayout());
-		Panel paymentPanel = new Panel(new BorderLayout());
+		Panel paymentPanel = new Panel(new GridLayout(0, 1, 0, 20));
+		Panel endPanel =  new Panel(new BorderLayout());
 		
-		// add comment
+		/* Put together the GUI objects into panels */
 		startPanel.add(welcomeLabel, BorderLayout.CENTER);
-		startPanel.add(btStOrder, BorderLayout.SOUTH);
 		
 		typePanel.add(typeLabel, BorderLayout.NORTH);
 		smooth = new String[] {"Acai Berry smoothie", "Strawberry smoothie", "Banana smoothie"};
 		smoothieType = new JComboBox(smooth);
+		((JLabel)smoothieType.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
 		typePanel.add(smoothieType, BorderLayout.SOUTH);
 		
 		sizePanel.add(sizeLabel, BorderLayout.NORTH);
 		size = new String[] {"Large", "Medium", "Small"};
 		sizeType = new JComboBox(size);
+		((JLabel)sizeType.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
 		sizePanel.add(sizeType, BorderLayout.SOUTH);
 		
 		sweetenerPanel.add(sweetenerLabel, BorderLayout.NORTH);
 		sweetener = new String[] {"Sugar", "Splenda", "No Sweetener"};
 		sweetType = new JComboBox(sweetener);
+		((JLabel)sweetType.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
 		sweetenerPanel.add(sweetType, BorderLayout.SOUTH);
-		
-		fruitPanel.add(fruitLabel, BorderLayout.NORTH);
-		fruit = new String[] {"Acai Berry", "Strawberry", "Banana"};
-		fruitType = new JComboBox(fruit);
-		fruitPanel.add(fruitType, BorderLayout.CENTER);
 		
 		addPanel.add(btAdd, BorderLayout.CENTER);
 
@@ -105,39 +106,62 @@ public class SmoothieShop extends Frame {
 		
 		payment = new String[] {"Credit Card", "Gift Card", "Cash"};
 		paymentType = new JComboBox(payment);
-		paymentPanel.add(paymentType, BorderLayout.CENTER);
+		((JLabel)paymentType.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+		paymentPanel.add(payLabel, BorderLayout.WEST);
+		paymentPanel.add(paymentType, BorderLayout.EAST);
 		paymentPanel.add(btPayProc, BorderLayout.SOUTH);
 
-		// add comment
-		Panel fieldFramePanel = new Panel(new GridLayout(0, 1, 1, 6));
+		endPanel.add(btStOrder);
+		
+		/* Put together the separate panels into the fieldFramePanel */
+		JPanel fieldFramePanel = new JPanel(new GridLayout(0, 1, 1, 1));
 		fieldFramePanel.add(startPanel);
 		fieldFramePanel.add(typePanel);
 		fieldFramePanel.add(sizePanel);
 		fieldFramePanel.add(sweetenerPanel);
-		//fieldFramePanel.add(fruitPanel);
 		fieldFramePanel.add(addPanel);
+		fieldFramePanel.add(endPanel);
 
-		/* Add, pack and show */
+		/* Put the higher level panels into the main javaFrame */
 		javaFrame.add(fieldFramePanel, BorderLayout.NORTH);
 		javaFrame.add(ordSumPanel, BorderLayout.CENTER);
 		javaFrame.add(paymentPanel, BorderLayout.SOUTH);
-		javaFrame.pack();
 		
+		btAdd.setFont(new Font("Serif", Font.BOLD, 18));
+		btStOrder.setFont(new Font("Serif", Font.BOLD, 18));
+		btPayProc.setFont(new Font("Serif", Font.BOLD, 20));
+		
+		/* Attach the action listeners to the buttons */
 		btAdd.addActionListener(new AddSmoothieListener());
 		btStOrder.addActionListener(new NewOrderListener());
 		btPayProc.addActionListener(new PayListener());
 		
-		// Close the window when close icon of the window is clicked
+		/* Close the window when close icon of the window is clicked */
 		javaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		/* Make the text areas not editable */
+		total.setEditable(false);
 		orderSummary.setEditable(false);
-		
+
+		javaFrame.pack();
 		javaFrame.setLocationRelativeTo(null);
 		javaFrame.setVisible(true);
-		
-		
+	
+		/* declare and define variable to pass be able to pass this object */
+		tempShop = this;
 	}
 	
+	/* Resets the main frame to prepare for a new order */
+	public void reset() {
+		orderSummary.setText("");
+		total.setText("");
+		preTaxTotal = 0;
+		postTaxTotal = 0;
+		
+		javaFrame.revalidate();
+	}
+	
+	/* Event listener, attached to btStOrder; Resets the main frame to prepare for a new order  */
 	class NewOrderListener implements ActionListener {
 		public void actionPerformed(ActionEvent click) {
 			orderSummary.setText("");
@@ -145,18 +169,19 @@ public class SmoothieShop extends Frame {
 			preTaxTotal = 0;
 			postTaxTotal = 0;
 			
-			javaFrame.setVisible(true);
+			javaFrame.revalidate();
 		}
 	}
 
-	// add comment
+	/* Event listener, attached to btAdd; Adds the current smoothie selections 
+	 * to the order summary and updates total cost */
 	class AddSmoothieListener implements ActionListener {
 		public void actionPerformed(ActionEvent click) {
 			String currentSmoothie = "";
 			String currentSize = "";
 			String currentSweetener = "";
 		
-			//add comment
+			/* Set the smoothie size */
 			if (sizeType.getSelectedItem() == size[0]) {
 				currentSize = size[0];
 			}
@@ -167,7 +192,7 @@ public class SmoothieShop extends Frame {
 				currentSize = size[2];
 			}
 			
-			// add comment
+			/* Set the sweetener type */
 			if (sweetType.getSelectedItem() == sweetener[0]) {
 				currentSweetener = sweetener[0];
 			}
@@ -178,7 +203,8 @@ public class SmoothieShop extends Frame {
 				currentSweetener = sweetener[2];
 			}
 			
-			// add comment
+			/* Set the smoothie type;  
+			 * Update preTaxTotal */
 			if (smoothieType.getSelectedItem() == smooth[0]) {
 				currentSmoothie = acaiBerrySmoothie.getDescription();
 				currentSmoothie += (acaiBerrySmoothie.cost(currentSize) + "\r\n");
@@ -195,56 +221,54 @@ public class SmoothieShop extends Frame {
 				preTaxTotal += bananaSmoothie.cost(currentSize);
 			}
 			
-			// add comment
-			
+			/* Add smoothie to orderSummary */
 			orderSummary.setEditable(true);
-			
 			orderSummary.append(currentSize + currentSmoothie);
 			orderSummary.append("           (" + currentSweetener + ")\r\n");
 			orderSummary.append("\r\n");
-			
 			orderSummary.setEditable(false);
 			
+			/* Update postTaxTotal */
 			postTaxTotal = addTax(preTaxTotal);
 			DecimalFormat pTT = new DecimalFormat("#.00");
 			String s = pTT.format(postTaxTotal);
+			total.setEditable(true);
+			total.setText("Total w/ tax =          $ " + s);
+			total.setEditable(false);
 			
-			total.setText("Total w/tax =          $ " + s);
-			javaFrame.setVisible(true);
+			/*  */
+			javaFrame.revalidate();
 		}
 	}
 	
 	class PayListener implements ActionListener {
 		public void actionPerformed(ActionEvent click) {
+			
 			if (paymentType.getSelectedItem() == payment[0]) {
 				
-				// Construct payment window
-				new ccPayWindow();
+				// Construct cc payment window
+				new ccPayWindow(postTaxTotal, tempShop);
 			}
 			if (paymentType.getSelectedItem() == payment[1]) {
 				
-				// Construct payment window
-				new gcPayWindow();
+				// Construct gc payment window
+				new gcPayWindow(postTaxTotal, tempShop);
 			}
 			if (paymentType.getSelectedItem() == payment[2]) {
 				
-				// Construct payment window
-				new cashPayWindow();
+				// Construct cash payment window
+				new cashPayWindow(postTaxTotal, tempShop);
 			}
 		}
 	}
 	
-	public void setFrameVisible() {
-		javaFrame.setVisible(true);
-	}
-	
-	public void setFrameNotVisible() {
-		javaFrame.setVisible(false);
-	}
-	
 	// add comment
 	static public void main(String argv[]) {
-		new SmoothieShop();
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	         public void run() {
+	        	 SmoothieShop smoothieShop = new SmoothieShop(); // Let the constructor do the job
+	         }
+	      });
 	}
 	
 	// add comment
